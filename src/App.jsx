93ffe3d6bd051f5e03b804541ace5561849a7fde
Javier Uribe <javieruribt@gamail.com>
2024-07-08 21:41:55 -0500

    Routes Configuratión

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
-import './App.css'
-import { Casquito1 } from './Components/Casquito1'
-import { Casquito2 } from './Components/Casquito2'
-import { Casquito3 } from './Components/Casquito3'
-import { Casquito4 } from './Components/Casquito4'
-import { Casquito5 } from './Components/Casquito5'
-import { Casquito6 } from './Components/Casquito6'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import './App.css';
+import { Casquito1 } from './Components/Casquito1';
+import { Casquito2 } from './Components/Casquito2';
+import { Casquito3 } from './Components/Casquito3';
+import { Casquito4 } from './Components/Casquito4';
+import { Casquito5 } from './Components/Casquito5';
+import { Casquito6 } from './Components/Casquito6';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Casquito1 />
-      <Casquito2 />
-      <Casquito3 />
-      <Casquito4 />
-      <Casquito5 />
-      <Casquito6 />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/mishi1' element = {<Casquito1 />} />
+          <Route path='/mishi2' element = {<Casquito2 />} />
+          <Route path='/mishi3' element = {<Casquito3 />} />
+          <Route path='/mishi4' element = {<Casquito4 />} />
+          <Route path='/mishi5' element = {<Casquito5 />} />
+          <Route path='/mishi6' element = {<Casquito6 />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
